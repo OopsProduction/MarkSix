e3d149911372cbc4c99910e3046be97a060d4019
Oops! Production <oopsproduction2012@gmail.com>
2017-10-22 22:47:23 +0800

    Test Suites created

---
+++ src/Test_addRecord/TestSuite_addRecord.java
@@ -0,0 +1,11 @@
+package Test_addRecord;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ test_addToDB.class, test_getNumbers.class })
+public class TestSuite_addRecord {
+
+}
---
+++ src/Test_randomNumbers/TestSuite_randomNumbers.java
@@ -0,0 +1,11 @@
+package Test_randomNumbers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ test_generateLuckyNumbers.class, test_genNum.class })
+public class TestSuite_randomNumbers {
+
+}
